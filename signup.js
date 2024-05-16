4ea47e5622c5cbf78d03443c6215bca7db4fcb99
Shashank Sharma <shashanksharma8868@gmail.com>
2024-05-16 23:21:47 +0530

    final project

--- signup.js
+++ signup.js
@@ -4,10 +4,11 @@ links = document.querySelectorAll(".link");
 
 links.forEach(link => {
 link.addEventListener("click", e => {
- e.preventDefault(); //preventing form submit
+ e.preventDefault(); 
  forms.classList.toggle("show-signup");
 })
 })
+
 
 
 
@@ -19,10 +20,9 @@ link.addEventListener("click", e => {
         const email = document.querySelector('.field input[type="email"]').value;
         const password = document.querySelector('.field input[type="password"]').value;
 
-        // Save the email and password in localStorage
         localStorage.setItem(email, password);
 
-        // Redirect to index.html
+
         window.location.href = 'index.html';
     });
 
@@ -37,12 +37,12 @@ link.addEventListener("click", e => {
         const email = document.querySelector('.input').value;
         const password = document.querySelector('.password').value;
 
-        // Check if the entered email exists in local storage
+
         const storedPassword = localStorage.getItem(email);
 
-        // Check if the entered password matches the stored password
+    
         if (storedPassword && password === storedPassword) {
-            // If matched, redirect to details page
+        
             window.location.href = 'main.html';
         } else {
             
